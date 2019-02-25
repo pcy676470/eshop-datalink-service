b7bead11264cfb2c82159b21a43b2ed71ff6ef6f
pcy676470 <961676871@qq.com>
2019-02-25 14:22:57 +0800

    33333333333333333

--- src/main/java/com/roncoo/eshop/datalink/web/controller/DataLinkController.java
+++ src/main/java/com/roncoo/eshop/datalink/web/controller/DataLinkController.java
@@ -28,22 +28,33 @@ public class DataLinkController {
 		Jedis jedis = jedisPool.getResource();
 		String dimProductJSON = jedis.get("dim_"+productId);
 		
+		System.out.println("==============1 dimProductJSON:"+dimProductJSON);
+		
 		if(dimProductJSON == null ||"".equals(dimProductJSON)){//主集群没有数据
 			String productDataJson = eshopProductService.findProductById(productId);
+			
+			System.out.println("==============2 productDataJson:"+productDataJson);
 			
 			if(productDataJson != null || !"".equals(productDataJson)){//数据库有数据
 				JSONObject productDataJSONObject = JSONObject.parseObject(productDataJson);
 				
+				
 				String productPropertyJSONDataJson = eshopProductService.findProductPropertyById(productId);
+				System.out.println("==============3 productPropertyJSONDataJson:"+productPropertyJSONDataJson);
+				
 				if(productPropertyJSONDataJson != null && !"".equals(productPropertyJSONDataJson)){
 					productDataJSONObject.put("product_property", JSONObject.parse(productPropertyJSONDataJson));
 				}
 				
 				String productSpecificationDataJSON = eshopProductService.findProductSpecificationById(productId);
+				
+				System.out.println("==============4 productSpecificationDataJSON:"+productSpecificationDataJSON);
+				
 				if(productSpecificationDataJSON != null && !"".equals(productSpecificationDataJSON)){
 					productDataJSONObject.put("product_specification", JSONObject.parse(productSpecificationDataJSON));
 				}
 				
+				System.out.println("==============5 productDataJSONObject.toJSONString():"+productDataJSONObject.toJSONString());
 				jedis.set("dim_product_" + productId, productDataJSONObject.toJSONString());
 				
 				return productDataJSONObject.toJSONString();
